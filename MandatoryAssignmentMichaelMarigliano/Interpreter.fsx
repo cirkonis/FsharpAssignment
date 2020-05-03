04f3147a6915236cd66bd32148b688011648c00e
michaelmarigliano <michael@sharkcell.dk>
2020-05-03 19:34:07 +0200

    if conditional for Interpreter

--- MandatoryAssignmentMichaelMarigliano/Interpreter.fsx
+++ MandatoryAssignmentMichaelMarigliano/Interpreter.fsx
@@ -31,8 +31,19 @@ let evaluateProgram (functionsList, argumentExpression) =
            | GE  (expression1, expression2)               -> if evaluate environment expression1 >= evaluate environment expression2
                                                                 then evaluate environment (INT(1)) 
                                                                 else evaluate environment (INT(0))
+           | AND (expression1, expression2)               -> if evaluate environment expression1 = 1 && evaluate environment expression2 = 1
+                                                                then evaluate environment (INT(1))
+                                                                else evaluate environment (INT(0))
+           | OR  (expression1, expression2)               -> if evaluate environment expression1 = 1 || evaluate environment expression2 = 1
+                                                                then evaluate environment (INT(1))
+                                                                else evaluate environment (INT(0))
+           | LET (x, expression1, expression2)            -> let var = evaluate environment expression1
+                                                             evaluate ((x, var) :: environment) expression2
+           | IF  (expression1, expression2, expression3)  -> if evaluate environment expression1 = 1 
+                                                             then evaluate environment expression2
+                                                             else evaluate environment expression3
         evaluate [] argumentExpression
         
         
-let example = parseProgFromString "4>=4"
+let example = parseProgFromString "let c = 7 in if c == 6 then 4 else 3"
 evaluateProgram example
