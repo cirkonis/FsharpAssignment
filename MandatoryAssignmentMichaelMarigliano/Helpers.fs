60f52eaf60bda34bc778128d2dbcc3869f0ce37d
michaelmarigliano <michael@sharkcell.dk>
2020-05-06 23:49:07 +0200

    got a good feeling about loopy

--- MandatoryAssignmentMichaelMarigliano/Helpers.fs
+++ MandatoryAssignmentMichaelMarigliano/Helpers.fs
@@ -1,5 +1,5 @@
 module Helpers
-//Used in Iterpreter Implementation     
+  
         let rec lookup x = function
               | []                      -> failwith ("unbound: " + x)
               | (y, w) :: environment   -> if x = y then w else lookup x environment
---
+++ MandatoryAssignmentMichaelMarigliano/Interpreter.fs
@@ -0,0 +1,50 @@
+module Interpreter
+//Implementation of the Interpreter
+
+let evaluateProgram (functionList, argumentExpression) =
+        let rec evaluate environment = function
+           | VAR x                                       -> lookup x environment
+           | INT i                                       -> i
+           | ADD (expression1, expression2)              -> evaluate environment expression1 + evaluate environment expression2
+           | SUB (expression1, expression2)              -> evaluate environment expression1 - evaluate environment expression2
+           | MUL (expression1, expression2)              -> evaluate environment expression1 * evaluate environment expression2
+           | DIV (expression1, expression2)              -> evaluate environment expression1 / evaluate environment expression2
+           | NEG expression                              -> evaluate environment expression * (-1)
+           | EQ  (expression1, expression2)              -> if evaluate environment expression1 = evaluate environment expression2
+                                                                then evaluate environment (INT(1)) 
+                                                                else evaluate environment (INT(0))
+           | NEQ (expression1, expression2)              -> if evaluate environment expression1 <> evaluate environment expression2
+                                                                then evaluate environment (INT(1)) 
+                                                                else evaluate environment (INT(0))
+           | LT  (expression1, expression2)               -> if evaluate environment expression1 < evaluate environment expression2
+                                                                then evaluate environment (INT(1)) 
+                                                                else evaluate environment (INT(0))
+           | LE  (expression1, expression2)               -> if evaluate environment expression1 <= evaluate environment expression2
+                                                                then evaluate environment (INT(1)) 
+                                                                else evaluate environment (INT(0))
+           | GT  (expression1, expression2)               -> if evaluate environment expression1 > evaluate environment expression2
+                                                                then evaluate environment (INT(1)) 
+                                                                else evaluate environment (INT(0))
+           | GE  (expression1, expression2)               -> if evaluate environment expression1 >= evaluate environment expression2
+                                                                then evaluate environment (INT(1)) 
+                                                                else evaluate environment (INT(0))
+           | AND (expression1, expression2)               -> if evaluate environment expression1 = 1 && evaluate environment expression2 = 1
+                                                                then evaluate environment (INT(1))
+                                                                else evaluate environment (INT(0))
+           | OR  (expression1, expression2)               -> if evaluate environment expression1 = 1 || evaluate environment expression2 = 1
+                                                                then evaluate environment (INT(1))
+                                                                else evaluate environment (INT(0))
+           | LET (x, expression1, expression2)            -> let var = evaluate environment expression1
+                                                             evaluate ((x, var) :: environment) expression2
+           | IF  (expression1, expression2, expression3)  -> if evaluate environment expression1 = 1 
+                                                             then evaluate environment expression2
+                                                             else evaluate environment expression3
+           | CALL (func, expression)                      -> if expression.Length <> 0
+                                                                then
+                                                                     let values = List.map(fun v -> evaluate environment v) expression
+                                                                     let (varnames, body) = lookup func functionList
+                                                                     let zippy = List.zip varnames values
+                                                                     evaluate zippy body
+                                                                else failwith "Function must have params"
+        evaluate [] argumentExpression
+
