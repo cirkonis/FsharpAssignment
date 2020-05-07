556c076c35c3f2483870e739e97ba1a07dfcd5f8
michaelmarigliano <michael@sharkcell.dk>
2020-05-07 10:59:28 +0200

    COMPILER

--- MandatoryAssignmentMichaelMarigliano/Compiler.fsx
+++ MandatoryAssignmentMichaelMarigliano/Compiler.fsx
@@ -1,141 +1,137 @@
 #load "VirtualMachine.fs"
-#load "Parser.fs"
-#load "Helpers.fs"
 open VM
-open Parser
-open Helpers
 
-//Implementation of the Compiler
-let rec compile functionEnvironment environment = function
- | INT i                             -> [IPUSH i]
- | ADD (expression1, expression2)    -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment ("" :: environment) expression2 @
+let mutable labelCounter = 0
+
+let newLabel _ = 
+ let this = labelCounter
+ labelCounter <- this + 1;
+ this
+ 
+let rec varPos x = function
+| []                -> failwith ("unbound: " + x)
+| y :: env  -> if x = y then 0 else 1 + varPos x env
+
+let rec comp funcs env = function
+ | INT i               -> [IPUSH i]
+ | ADD (exp1, exp2)    -> comp funcs env         exp1 @
+                                        comp funcs ("" :: env) exp2 @
                                         [IADD]
- | SUB (expression1, expression2)    -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment ("" :: environment) expression2 @
+ | SUB (exp1, exp2)    -> comp funcs env         exp1 @
+                                        comp funcs ("" :: env) exp2 @
                                         [ISUB]
- | MUL (expression1, expression2)    -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment ("" :: environment) expression2 @
+ | MUL (exp1, exp2)    -> comp funcs env         exp1 @
+                                        comp funcs ("" :: env) exp2 @
                                         [IMUL]
- | DIV (expression1, expression2)    -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment ("" :: environment) expression2 @
+ | DIV (exp1, exp2)    -> comp funcs env         exp1 @
+                                        comp funcs ("" :: env) exp2 @
                                         [IDIV]
- | NEG (expression)                  -> compile functionEnvironment environment (INT(0)) @
-                                        compile functionEnvironment ("" :: environment) expression  @
+ | NEG (exp)                  -> comp funcs env (INT(0)) @
+                                        comp funcs ("" :: env) exp @
                                         [ISUB]
- | VAR x                             -> [IGET (variablePosition x environment)]
- | LET (x, expression1, expression2) -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment (x :: environment)  expression2 @
+ | VAR x                             -> [IGET (varPos x env)]
+ | LET (x, exp1, exp2) -> comp funcs env         exp1 @
+                                        comp funcs (x :: env)  exp2 @
                                         [ISWAP]@
                                         [IPOP]
- | EQ (expression1, expression2)     -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment ("" :: environment) expression2 @
+ | EQ (exp1, exp2)     -> comp funcs env         exp1 @
+                                        comp funcs ("" :: env) exp2 @
                                         [IEQ]
- | NEQ (expression1, expression2)    -> compile functionEnvironment environment (INT(1)) @
-                                        compile functionEnvironment ("" :: environment) (EQ(expression1, expression2)) @
+ | NEQ (exp1, exp2)    -> comp funcs env (INT(1)) @
+                                        comp funcs ("" :: env) (EQ(exp1, exp2)) @
                                         [ISUB]
- | LT (expression1, expression2)     -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment ("" :: environment) expression2 @
+ | LT (exp1, exp2)     -> comp funcs env         exp1 @
+                                        comp funcs ("" :: env) exp2 @
                                         [ILT]
- | LE (expression1, expression2)     -> compile functionEnvironment environment         expression1 @
-                                        compile functionEnvironment ("" :: environment) expression2 @
+ | LE (exp1, exp2)     -> comp funcs env         exp1 @
+                                        comp funcs ("" :: env) exp2 @
                                         [ILE]
- | GT (expression1, expression2)     -> compile functionEnvironment environment         expression2 @
-                                        compile functionEnvironment ("" :: environment) expression1 @
+ | GT (exp1, exp2)     -> comp funcs env         exp2 @
+                                        comp funcs ("" :: env) exp1 @
                                         [ILT]
- | GE (expression1, expression2)     -> compile functionEnvironment environment         expression2 @
-                                        compile functionEnvironment ("" :: environment) expression1 @
+ | GE (exp1, exp2)     -> comp funcs env         exp2 @
+                                        comp funcs ("" :: env) exp1 @
                                         [ILE]
- | IF (expression1, expression2, expression3)    -> let label2 = newLabel()
-                                                    let label1 = newLabel()
-                                                    compile functionEnvironment environment expression1 @
-                                                    [IJMPIF label2]                 @
-                                                    compile functionEnvironment environment expression3 @
-                                                    [IJMP label1]                   @
-                                                    [ILAB label2]                   @
-                                                    compile functionEnvironment environment expression2 @
-                                                    [ILAB label1]
- | AND (expression1, expression2)    -> compile functionEnvironment environment (INT(2))@
-                                        compile functionEnvironment ("" :: environment) (expression1) @
-                                        compile functionEnvironment ("" :: environment) (expression2) @
+ | IF (exp1, exp2, exp3)    ->  let label2 = newLabel()
+                                let labele = newLabel()
+                                comp funcs env exp1 @
+                                [IJMPIF label2]                 @
+                                comp funcs env exp3 @
+                                [IJMP labele]                   @
+                                [ILAB label2]                   @
+                                comp funcs env exp2 @
+                                [ILAB labele]
+ | AND (exp1, exp2)    -> comp funcs env (INT(2))@
+                                        comp funcs ("" :: env) (exp1) @
+                                        comp funcs ("" :: env) (exp2) @
                                         [IADD]@
                                         [IEQ]
- | OR (expression1, expression2)    ->  compile functionEnvironment environment (INT(0)) @ 
-                                        compile functionEnvironment ("" :: environment) (expression1) @
-                                        compile functionEnvironment ("" :: environment) (expression2) @
+ | OR (exp1, exp2)    ->  comp funcs env (INT(0)) @ 
+                                        comp funcs ("" :: env) (exp1) @
+                                        comp funcs ("" :: env) (exp2) @
                                         [IADD] @
                                         [ILT]
- | CALL (funkshun, expression)      ->  let labReturn = newLabel()
-                                        let labFunc = lookup funkshun functionEnvironment
+ | CALL (func, exp)                 ->  let labr = newLabel()
+                                        let labf = lookup func funcs
                                         let loop =
-                                            let rec loopy expression = 
-                                                      match expression with
+                                            let rec exps exp = 
+                                                      match exp with
                                                       | [] -> []
                                                       | e::exp -> 
-                                                         compile functionEnvironment ("" :: environment) e @
-                                                         loopy exp
-                                            loopy expression
+                                                         comp funcs ("" :: env) e @
+                                                         exps exp
+                                            exps exp
                                         loop @    
-                                        [ICALL labFunc] @
-                                        [ILAB  labReturn] @
+                                        [ICALL labf] @
+                                        [ILAB  labr] @
                                         [ISWAP] @
                                         [IPOP]
                                         
 
-let compileProgram (listOfFunctions, expression) = 
-        let functionEnvironment = List.map (fun (f, _) -> (f, newLabel())) listOfFunctions
-        let rec compileFunctions = function
-          | []                               -> compile functionEnvironment [] expression @
+let compProg (funcs, exp) = 
+        let funcEnv = List.map (fun (f, _) -> (f, newLabel())) funcs
+        let rec compFuncs = function
+          | []                               -> comp funcEnv [] exp @
                                                 [IHALT]
-          | (f, (x, e)) :: listOfFunctions   -> let functionLabel = lookup f functionEnvironment
-                                                compileFunctions listOfFunctions      @
-                                                [ILAB functionLabel]@
-                                                compile functionEnvironment ["";x] e  @
+          | (f, (x, e)) :: funcs             -> let labelf = lookup f funcEnv
+                                                compFuncs funcs      @
+                                                [ILAB labelf]@
+                                                comp funcEnv ["";x] e  @
                                                 [ISWAP]@
                                                 [IRETN]
-        compileFunctions listOfFunctions
+        compFuncs funcs
 
                                         
-//Testing
-//Individual Component Testing
-let compileSimpleAdd = compile [] [] (ADD(INT 1, INT 2)) 
-let CompiledSimpleAddAnswer = execProg compileSimpleAdd [] //Should be 3
-let compileSimpleSub = compile [] [] (SUB(INT 1, INT 2))
-let CompiledSimpleSubAnswer = execProg compileSimpleSub [] //Should be -1
-let compileSimpleMul = compile [] [] (MUL(INT 5, INT 5))
-let CompiledSimpleMulAnswer = execProg compileSimpleMul [] //Should be 25
-let compileSimpleDiv = compile [] [] (DIV(INT 0, INT 5)) 
-let CompiledSimpleDivAnswer = execProg compileSimpleDiv [] //Should be 0. Testing Div by zero results in error
-let compileSimpleNeg = compile [] [] (NEG(INT 4))
-let CompiledSimpleNegAnswer = execProg compileSimpleNeg [] //Should be -4
-let compileSimpleEq = compile [] [] (EQ(INT 4, INT 3)) 
-let CompiledSimpleEqAnswer = execProg compileSimpleEq [] //Should be False(0)
-let compileSimpleNeq = compile [] [] (NEQ(INT 4, INT 3)) 
-let CompiledSimpleNeqAnswer = execProg compileSimpleNeq [] //Should be True(1)
+let compAdd = comp [] [] (ADD(INT 1, INT 1)) 
+let Add = execProg compAdd [] 
+let compSub = comp [] [] (SUB(INT 1, INT 0))
+let Sub = execProg compSub [] 
+let compMul = comp [] [] (MUL(INT 1, INT 1))
+let Mul = execProg compMul [] 
+let compDiv = comp [] [] (DIV(INT 1, INT 1)) 
+let Div = execProg compDiv [] 
+let compNeg = comp [] [] (NEG(INT 1))
+let Neg = execProg compNeg [] 
+let compEq = comp [] [] (EQ(INT 1, INT 2)) 
+let Eq = execProg compEq [] 
+let compNeq = comp [] [] (NEQ(INT 10, INT 10)) 
+let Neq = execProg compNeq [] 
+let compLet = comp [] [] (LET("y", INT(5), SUB(VAR "y", INT 4)))
+let Let = execProg compLet [] 
+let compGe = comp [] [] (GE(INT(5), INT(6)))
+let Ge = execProg compGe [] 
+let compGt = comp [] [] (GT(INT(1), INT(3)))
+let Gt = execProg compGt [] 
+let compLe = comp [] [] (LE(INT(2), INT(1)))
+let Le = execProg compLe []
+let compLt = comp [] [] (LT(INT(1), INT(1)))
+let Lt = execProg compLt [] 
+let compIfAnd = comp [] [] (IF(AND(EQ(INT(5), INT(5)),EQ(INT(4),INT(4))),INT(1),INT(0)))
+let IfAnd = execProg compIfAnd [] 
+let compIfOr = comp [] [] (IF(OR(EQ(INT(5), INT(5)),EQ(INT(3),INT(4))),INT(1),INT(0)))
+let IfOr = execProg compIfOr [] 
+//from doc
+let compFunction = compProg([("Add2", ("x", ADD(INT 2, VAR "x")))], (CALL("Add2", [INT 5])))
+let Function = execProg compFunction []
 
-let compileSimpleLet = compile [] [] (LET("x", INT(5), ADD(VAR "x", INT 5)))
-let CompiledSimpleLetAnswer = execProg compileSimpleLet [] //Should be 10
-let compileSimpleGe = compile [] [] (GE(INT(6), INT(5)))
-let CompiledSimpleGeAnswer = execProg compileSimpleGe [] //Should be True(1)
-let compileSimpleGt = compile [] [] (GT(INT(4), INT(5)))
-let CompiledSimpleGtAnswer = execProg compileSimpleGt [] //Should be False(1)
-let compileSimpleLe = compile [] [] (LE(INT(5), INT(5)))
-let CompiledSimpleLeAnswer = execProg compileSimpleLe [] //Should be True(1)
-let compileSimpleLt = compile [] [] (LT(INT(4), INT(5)))
-let CompiledSimpleLtAnswer = execProg compileSimpleLt [] //Should be True(1)
-
-let compileSimpleIfAnd = compile [] [] (IF(AND(EQ(INT(5), INT(5)),EQ(INT(4),INT(4))),INT(1),INT(0)))
-let CompiledSimpleIfAndAnswer = execProg compileSimpleIfAnd [] //Should be True(1)
-let compileSimpleIfOr = compile [] [] (IF(OR(EQ(INT(5), INT(5)),EQ(INT(3),INT(4))),INT(1),INT(0)))
-let CompiledSimpleIfOrAnswer = execProg compileSimpleIfOr [] //Should be True(1)
-
-//FunctionTesting
-let compileFunctionSingleArgument = compileProgram([("Add2", ("x", ADD(INT 2, VAR "x")))], (CALL("Add2", [INT 5])))
-let compileFunctionSingleArgumentAnswer = execProg compileFunctionSingleArgument []
-//FactorialTest error with indexing
-//let compileFactorial = compileProgram ([("fac", ("n",IF (GT (VAR "n",INT 0),MUL (VAR "n",CALL ("fac",[SUB (VAR "n",INT 1)])), INT 1)))], (CALL ("fac",[INT 5])))
-//let compileFactorialAnswer = execProg compileFactorial []
-//Multiple Argurment Function Not Implemented
-//let compileFunctionMultipleArguments = compileProgram([("f", ("x, y", ADD(VAR "y", VAR "x")))], (CALL("f", [INT 5, INT 4])))
-//let compileFunctionMultipleArgumentsAnswer = execProg compileFunctionMultipleArguments
     
